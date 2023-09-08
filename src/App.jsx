70ad5aac094e2f99d0ca265a1471edf275520a5f
Jagroshan <jagroshansingh615@gmail.com>
2023-09-08 18:42:51 +0530

    serch functionality working

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import axios from "axios";
 import "./App.css";
 import { RiNftFill } from "react-icons/ri";
 import { MdOutlineToken } from "react-icons/md";
+import { AiOutlineSearch } from "react-icons/ai";
 import { Card } from "./components/Card";
 
 function App() {
   const [data, setData] = React.useState([]);
-  const [search,setSearch]=React.useState("");
-  console.log(search)
-  // console.log(data);
+  const [search, setSearch] = React.useState("");
+  const [type, setType]=React.useState("Token")
+  console.log(search);
+
+  function fetchData() {
+    axios({
+      method: "get",
+      url: `https://api.dexscreener.com/latest/dex/tokens/${search}`,
+    })
+      .then((res) => setData(res.data.pairs))
+      .catch((err) => console.log(err));
+  }
 
   const handleClick = (el) => {
     let token = document.getElementById("token");
@@ -17,25 +27,28 @@ function App() {
     if (el.target.innerText == "Token Address") {
       token.style.backgroundColor = "#F30050";
       pair.style.backgroundColor = "transparent";
+      setType('Token')
     } else {
       token.style.backgroundColor = "transparent";
       pair.style.backgroundColor = "#F30050";
+      setType('Pair')
     }
   };
 
-  const handleSearch=(el)=>{
-    setSearch(el.target.value)
-    console.log(el)
-  }
+  const handleInput = (el) => {
+    setSearch(el.target.value);
+  };
 
-  React.useEffect(() => {
-    axios({
-      method: "get",
-      url: `https://api.dexscreener.com/latest/dex/tokens/${search}`,
-    })
-      .then((res) => setData(res.data.pairs))
-      .catch((err) => console.log(err));
-  }, []);
+  const handleSearch = (el) => {
+    fetchData();
+  };
+
+  //Fetching data through Enter keypress
+  const handleEnter = (el) => {
+    if (el.key == "Enter") fetchData();
+  };
+
+  React.useEffect(() => {}, []);
   return (
     <div>
       <div id="mainBody">
@@ -57,19 +70,30 @@ function App() {
         </div>
         <div id="mainPanel">
           <div id="navbar">
-            <input type="search" name="" id="" placeholder="Search" onChange={handleSearch}/>
+            <div>
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Search"
+                onChange={handleInput}
+                onKeyDown={handleEnter}
+              />
+              <button id="search" onClick={handleSearch}>
+                <AiOutlineSearch />
+              </button>
+            </div>
             <button>Connect</button>
           </div>
-          <h3>Token Search Results</h3>
+          <h3>{type} Search Results</h3>
           <div id="cardsContainer">
             {data?.map((each, i) => (
               <Card key={i} details={each} />
             ))}
-            {/* <Card/> */}
           </div>
         </div>
       </div>
-      <div id="Footer"></div>
+      <div id="footer"></div>
       <img
         src="https://images.deccanherald.com/deccanherald%2F2023-08%2F1eb13648-1e78-4cf6-bf57-89a89023a08f%2F752.png?auto=format%2Ccompress&fmt=webp&fit=max&format=webp&w=900&dpr=1.3"
         alt="background"
