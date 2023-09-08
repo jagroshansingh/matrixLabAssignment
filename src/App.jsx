1c19b5c8be30dad6be2cc6456f32bc4a1f049080
Jagroshan <jagroshansingh615@gmail.com>
2023-09-08 21:52:25 +0530

    made responside, api data is trimmed and showing in decending order of price

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,27 @@ import "./App.css";
 import { RiNftFill } from "react-icons/ri";
 import { MdOutlineToken } from "react-icons/md";
 import { AiOutlineSearch } from "react-icons/ai";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { Card } from "./components/Card";
 
 function App() {
   const [data, setData] = React.useState([]);
   const [search, setSearch] = React.useState("");
-  const [type, setType]=React.useState("Token")
-  console.log(search);
+  const [type, setType] = React.useState("Token");
+  const [api,setApi]=React.useState('https://api.dexscreener.com/latest/dex/tokens/')
+  
+
+  data.sort((a,b)=>b.priceUsd-a.priceUsd)    // for putting in decending order 
+  console.log(data)
 
   function fetchData() {
     axios({
       method: "get",
-      url: `https://api.dexscreener.com/latest/dex/tokens/${search}`,
+      url: `${api}${search}`,
     })
-      .then((res) => setData(res.data.pairs))
+      .then((res) => {
+        let slice=res.data.pairs.slice(0,10)  //for trimming the responce to only 10 elements
+        setData(slice)})
       .catch((err) => console.log(err));
   }
 
@@ -27,11 +34,13 @@ function App() {
     if (el.target.innerText == "Token Address") {
       token.style.backgroundColor = "#F30050";
       pair.style.backgroundColor = "transparent";
-      setType('Token')
+      setType("Token");
+      setApi('https://api.dexscreener.com/latest/dex/tokens/')
     } else {
       token.style.backgroundColor = "transparent";
       pair.style.backgroundColor = "#F30050";
-      setType('Pair')
+      setType("Pair");
+      setApi('https://api.dexscreener.com/latest/dex/search/?q=')
     }
   };
 
@@ -48,7 +57,9 @@ function App() {
     if (el.key == "Enter") fetchData();
   };
 
-  React.useEffect(() => {}, []);
+  React.useEffect(() => {
+    setData([])
+  }, [api]);
   return (
     <div>
       <div id="mainBody">
@@ -98,6 +109,10 @@ function App() {
         src="https://images.deccanherald.com/deccanherald%2F2023-08%2F1eb13648-1e78-4cf6-bf57-89a89023a08f%2F752.png?auto=format%2Ccompress&fmt=webp&fit=max&format=webp&w=900&dpr=1.3"
         alt="background"
       />
+      <div className="logo" id="nft">
+        <RxHamburgerMenu/>
+        <p>NFTify</p>
+      </div>
     </div>
   );
 }
